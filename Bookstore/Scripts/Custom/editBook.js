0aa99239eb1a4247cd7628a92c3c781e9b2471da
Nickose777 <my_personal@mail.ua>
2017-06-28 00:11:34 +0300

    editBook.js included

---
+++ Bookstore/Scripts/Custom/editBook.js
@@ -0,0 +1,14 @@
+﻿function editBook(encryptedId) {
+    $.ajax({
+        url: "/Book/Get/" + encryptedId,
+        dataType: "html",
+        type: "GET",
+        success: function (data) {
+            $("#modalContainer").html(data);
+            $("#modal").modal();
+        },
+        error: function (error) {
+            alert(error.statusText + " (" + error.status + ")");
+        }
+    });
+}
